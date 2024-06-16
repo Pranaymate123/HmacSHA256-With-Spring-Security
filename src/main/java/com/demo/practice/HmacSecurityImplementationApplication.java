244af9fd35174ecff850e0bb9922cf1200ebd61a
Pranaymate123 <pranaymate0706@gmail.com>
2024-06-16 10:04:09 +0530

    initial commit

---
+++ src/main/java/com/demo/practice/HmacSecurityImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.demo.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmacSecurityImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmacSecurityImplementationApplication.class, args);
+	}
+
+}
